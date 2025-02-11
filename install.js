90efcf1784fd16fdcbd4ee6af9b910ba3246a08d
cocktailpeanut <cocktailpeanuts@proton.me>
2025-02-11 04:04:16 -0500

    update

--- install.js
+++ install.js
@@ -28,7 +28,8 @@ module.exports = {
         venv: "env",                // Edit this to customize the venv folder path
         path: "app",                // Edit this to customize the path to start the shell from
         message: [
-          "uv pip install -e ."
+          "uv pip install -e .",
+          "uv pip install -e .[compile]"
         ]
       }
     },
