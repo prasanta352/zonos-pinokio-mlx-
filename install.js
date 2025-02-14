8022a57b03c0bc01125360384e37e18518461cad
cocktailpeanut <cocktailpeanuts@proton.me>
2025-02-14 15:33:31 -0500

    update

--- install.js
+++ install.js
@@ -47,8 +47,8 @@ module.exports = {
         message: [
           //"uv pip install mamba-ssm>=2.2.4",
           //"uv pip install git+https://github.com/Dao-AILab/causal-conv1d --no-build-isolation"
-          "uv pip install git+https://github.com/sdbds/Zonos-for-windows/releases/download/windows-wheel/mamba_ssm-2.2.4-cp310-cp310-win_amd64.whl",
-          "uv pip install git+https://github.com/sdbds/Zonos-for-windows/releases/download/windows-wheel/causal_conv1d-1.5.0.post8-cp310-cp310-win_amd64.whl"
+          "uv pip install https://github.com/sdbds/Zonos-for-windows/releases/download/windows-wheel/mamba_ssm-2.2.4-cp310-cp310-win_amd64.whl",
+          "uv pip install https://github.com/sdbds/Zonos-for-windows/releases/download/windows-wheel/causal_conv1d-1.5.0.post8-cp310-cp310-win_amd64.whl"
         ]
       }
     },
