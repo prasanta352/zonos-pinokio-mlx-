e8213ee3d4ed3c889d3d40dcd3522e3dd20b87f3
cocktailpeanut <cocktailpeanuts@proton.me>
2025-02-15 11:36:35 -0500

    update

--- start.js
+++ start.js
@@ -2,7 +2,8 @@ const fg = require('fast-glob');
 const path = require('path')
 module.exports = async (kernel) => {
   let env = {
-    SERVER_NAME: "127.0.0.1"
+    SERVER_NAME: "127.0.0.1",
+    PYTHONUTF8: 1
   }
   if (kernel.platform === 'darwin') {
     try {
