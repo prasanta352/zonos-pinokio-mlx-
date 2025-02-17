184cac4d83c85cf1ae915b1918c78ab7f1342787
cocktailpeanut <cocktailpeanuts@proton.me>
2025-02-17 11:47:38 -0500

    update

--- install.js
+++ install.js
@@ -1,19 +1,19 @@
 const os = require('os')
 const fs = require('fs')
-const { rimraf } = require('rimraf')
+//const { rimraf } = require('rimraf')
 const path = require('path')
 const exists = (abspath) => {
   return new Promise(r=>fs.access(abspath, fs.constants.F_OK, e => r(!e)))
 }
 module.exports = async (kernel) => {
-  const homedir = os.homedir()
-  const triton_cache_dir = path.resolve(homedir, ".triton/cache")
-  let e = await exists(triton_cache_dir)
-  if (e) {
-    console.log("delete cache")
-    await rimraf(triton_cache_dir)
-  }
-  console.log("rimraf", { triton_cache_dir, e })
+//  const homedir = os.homedir()
+//  const triton_cache_dir = path.resolve(homedir, ".triton/cache")
+//  let e = await exists(triton_cache_dir)
+//  if (e) {
+//    console.log("delete cache")
+//    await rimraf(triton_cache_dir)
+//  }
+//  console.log("rimraf", { triton_cache_dir, e })
   return {
     run: [
       // Edit this step to customize the git repository to use
